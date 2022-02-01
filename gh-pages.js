772c97dbb8be94182a5fdf6f883f028092c3327e
Annie Fu <afu@industrydive.com>
2022-02-01 15:30:18 -0500

    edit

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,7 @@ ghpages.publish(
     'public', // path to public directory
     {
         branch: 'master',
-        repo: 'https://github.com/annieefu/chinatown.git', // Update to point to your repository  
+        repo: 'https://github.com/annieefu/det-chinatown.git', // Update to point to your repository  
         user: {
             name: 'Annie Fu', // update to use your name
             email: 'annieccfu@gmail.com' // Update to use your email
